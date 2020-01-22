6ec177f1cbc1bd3a0df9f2e1c67f56997016fe39
Alexei Shilin <alexei.shilin@gmail.com>
2020-01-22 13:53:21 +0700

    complete count-types

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = array => {
+  const typesCount = {};
+  for (const val of array) {
+    const type = typeof val;
+    const count = typesCount[type] || 0;
+    typesCount[type] = count + 1;
+  }
+  return typesCount;
+};
 
 module.exports = { countTypesInArray };
